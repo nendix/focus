3b42e4601eb3052ed3799e8d2a5aa3937a3236a3
nendix <nendix12@gmail.com>
2025-09-24 14:04:43 +0200

    fix: version print when installing with go install

--- cmd/focus/main.go
+++ cmd/focus/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	tea "github.com/charmbracelet/bubbletea"
 
@@ -13,11 +14,29 @@ var (
 	version = "dev"
 )
 
+// getVersion returns the version string, preferring build-time ldflags
+// but falling back to runtime/debug.BuildInfo for go install
+func getVersion() string {
+	// If version was set via ldflags (make build), use that
+	if version != "dev" {
+		return version
+	}
+
+	// Otherwise, try to get version from build info (go install)
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "(devel)" && info.Main.Version != "" {
+			return info.Main.Version
+		}
+	}
+
+	return "dev"
+}
+
 func main() {
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "--version", "-v":
-			fmt.Printf("focus %s\n", version)
+			fmt.Printf("focus %s\n", getVersion())
 			return
 		case "--help", "-h":
 			fmt.Println("Focus - A minimalist pomodoro timer")
